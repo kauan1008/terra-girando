77a26a659c50460e4d461a2734ed82ae936e2e4d
kauan1008 <kauan.gabriel.silva10@escola.pr.gov.br>
2026-03-11 15:14:48 -0300

    sketch123

--- sketch.js
+++ sketch.js
@@ -1,13 +1,16 @@
-function setup() {
+let terra;
+
+function preoload(){
+    terra = loadImage('terra.jpj')
+}
+}function setup() {
     creatcanvas(1920, 1080, webgl);//plano 3D
-    terra=loadImag'terra.jpg')
 }
 
 function draw( ){
     backgraud(205, 109, 94);//cor de fundo
     rotateY(millis()/1000);//rotação da terra
-    TextTrackCue(terra);//da terra, imagem.
+    textTrackCue(terra);//da terra, imagem.
     Spehere(80, 100);//esfera 3D
 
-}
 
