93af10a4d71bd5e41e1261d295867b300ed5505f
kauan1008 <kauan.gabriel.silva10@escola.pr.gov.br>
2026-03-18 15:13:52 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,11 @@
 function setup() {
-    creatcanvas(1920, 1080, webgl);//plano 3D
-terra=loadImage('terra.jpg')
+    createCanvas(1920,1080, WEBGL);//plano 3D
+    terra=loadImage('terra.jpg')
 }
 
-function draw( ){
-    backgraud(205, 109, 94);//cor de fundo
+function draw() {
+    background(205, 102, 94);//cor de fundo
     rotateY(millis()/1000);//rotação da terra
-    textTrackCue(terra);//da terra, imagem.
-    Spehere(80, 100);//esfera 3D
+    texture(terra);//textura da terra, imagem.
+    sphere(80, 100);//esfera 3D
 }
